44582cd0e74e7437b1e24ac8dba3f6e0ce3adbba
bahaehmimdi <48775632+bahaehmimdi@users.noreply.github.com>
2024-05-01 11:35:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -216,7 +216,7 @@ def get_html(url):
 
 
 
-@app.route('/<username>')
+@app.route('/<path:subpath>')
 def tasktest(name):
  try:   
   return get_html_text(request.url.replace(r"https://test2-bblm.onrender.com/",""))
