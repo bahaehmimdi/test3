d290306feb8f9d27a909140245203ace5523c1ea
bahaehmimdi <48775632+bahaehmimdi@users.noreply.github.com>
2024-05-30 17:49:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -254,45 +254,7 @@ def get_html(url):
     return "nothing worked"
 
 
-def google_search(query):
-    # Replace spaces with plus signs for the query string
-    query = query.replace(' ', '+')
-    url = f'https://www.google.com/search?q=site:societe.com "{query}"'
-   
-    # Set the User-Agent to mimic a real browser request
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-    }
 
-    # Send the request to Google
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-
-    # Parse the HTML content
-   # print(response.text)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    # Extract the first search result title, snippet, and link
-    first_result = {}
-    g = soup.find("h3")
-    
-    if g:
-  
-        title = g.find('h3')
-       
-        if title:
-            title = title.text
-            link = g.find('a', href=True)['href']
-            snippet = g.find('span', class_='aCOpRe')
-            if snippet:
-                snippet = snippet.text
-            first_result = {'title': title, 'link': link, 'snippet': snippet}
-    
-    return g.text.split("(")[0]
-@app.route('/deno/<bahae>')
-def scrap(bahae):
- 
-      return google_search(bahae)
 
 @app.route('/<path:subpath>')
 def tasktest(subpath):
